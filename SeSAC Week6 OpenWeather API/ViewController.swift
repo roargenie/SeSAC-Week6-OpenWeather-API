740632f96df7fcea0c5674f736b11a71886a9a1f
roargenie <105975078+roargenie@users.noreply.github.com>
2022-08-16 16:18:04 +0900

    Initial Commit

---
+++ SeSAC Week6 OpenWeather API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC Week6 OpenWeather API
+//
+//  Created by 이명진 on 2022/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
